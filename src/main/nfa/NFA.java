698e2f3042f1afbfa8b3b7d7650b9a47632082d0
kirakira0 <kiratoal0@gmail.com>
2020-12-18 16:33:40 -0800

    First commit

---
+++ src/main/nfa/NFA.java
@@ -0,0 +1,237 @@
+/**
+ *
+ * @author Kira Toal
+ */ 
+
+package main.nfa;
+
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class NFA {
+
+    private HashSet<Character> alphabet; 
+    private HashMap<State, HashMap<Character, HashSet<State>>> data; 
+
+
+    public NFA(HashSet<Character> alphabet, HashMap<State, HashMap<Character, HashSet<State>>> data) {
+        this.alphabet = alphabet;
+        this.data = data;
+    }
+    
+    public void addState(State state) {
+        if (this.data.containsKey(state)) {
+            throw new IllegalArgumentException("State already exists."); 
+        }
+        this.data.put(state, new HashMap<Character, HashSet<State>>());         
+    }
+    
+    
+    public void addTransition(State state, Transition transition) {
+        // Ensure symbol in alphabet. 
+        if (!this.alphabet.contains(transition.symbol)) {
+            throw new IllegalArgumentException("Transition symbol not in alphabet.");                         
+        }
+        // Ensure state in NFA. 
+        if (!this.data.containsKey(state)) {
+            throw new IllegalArgumentException("Cannot add tranistion to nonexistant state.");             
+        }
+        // Add new transition. 
+        if (!this.data.get(state).containsKey(transition.symbol)) {
+            this.data.get(state).put(transition.symbol, new HashSet<State>(Arrays.asList(transition.dest)));
+        }
+        // Add state to transition.
+        else {
+            // Do not add duplicate transition. 
+            if (this.data.get(state).get(transition.symbol).contains(transition.dest)) {
+                return;
+            }
+            HashSet<State> oldStatesSet = this.data.get(state).get(transition.symbol);
+            oldStatesSet.add(transition.dest);         
+        }                  
+
+    }
+    
+    /**
+     * Determined whether an NFA is a DFA. In order for this to be true,
+     * the following conditions must be satisfied: 
+     *  (1) The alphabet may not contain λ (no λ transitions allowed)/
+     *  (2) Each state must have a transition for every character in the alphabet.
+     *  (3) Every symbol must have only one destination state.
+     * @return Whether or not a given NFA is also an DFA.
+     */
+    public boolean isDFA() {
+        // Condition 1: DFA cannot use lambda moves. 
+        if (this.alphabet.contains('λ')) {return false;}
+        for (State state: this.data.keySet()) {
+            for (Character c: this.alphabet) {
+                // Condition 2: DFA must have transition for every char
+                //  in the alphabet. 
+                if (!this.data.get(state).keySet().contains(c)) {
+                    return false; 
+                }
+            }
+            for (Character symbol: this.data.get(state).keySet()) {
+                 // Condition 3: Each transition symbol must have only one 
+                 //   destination state.
+                 if (this.data.get(state).get(symbol).size() > 1) {
+                     return false;
+                 }         
+             }                   
+        }       
+        return true;       
+    }
+    
+    /**
+     * 
+     */
+//    public NFA minimize() {
+    public void minimize() {
+
+        // K equivalence 
+        HashSet<HashSet<State>> stateGroups = new HashSet<HashSet<State>>();
+        HashSet<State> acceptStates = new HashSet<State>();
+        HashSet<State> nonAcceptStates = new HashSet<State>();
+        for (State state: this.data.keySet()) {
+            if (state.accept)  {
+                acceptStates.add(state);
+            } else {
+                nonAcceptStates.add(state);
+            }
+        }
+        stateGroups.add(acceptStates);
+        stateGroups.add(nonAcceptStates);
+        System.out.println("------------------->>>"); 
+        this.kEquivalenceHelper(stateGroups); 
+
+    }
+    
+    
+    /**
+     * Sort the states of the DFA into groups using the k equivalence method. 
+     * @param stateGroups The states should originally be split into two groups:
+     *      one for the accept states and one for the non-accept states. 
+     * @return The correct sorting of the groups. 
+     */
+    public HashSet<HashSet<State>> kEquivalenceHelper(HashSet<HashSet<State>> stateGroups) {
+        var newGroups = new HashSet<HashSet<State>>();
+        for (var group: stateGroups) {
+            var keep = new HashSet<State>();
+            var leave = new HashSet<State>();
+            for (var state: group) {
+                if (this.stateExits(state, group)) {
+                    leave.add(state);
+                } else {
+                    keep.add(state);
+                }
+            }
+            if (!leave.isEmpty()) {
+                newGroups.add(leave);
+            }
+            if (!keep.isEmpty()) {
+                newGroups.add(keep);
+            }
+        }
+        // this.printStateGroup(newGroups);
+        return stateGroups.equals(newGroups) ? newGroups : this.kEquivalenceHelper(newGroups);       
+    }
+    
+    
+    /**
+     * 
+     * @param groups
+     */
+    private void printStateGroup(HashSet<HashSet<State>> groups) {
+        String result = "Groups: ";
+        for (HashSet<State> group: groups) {
+            result += "[ ";
+            for (State state: group) {
+                result += " " + state.name + " ";
+            }
+            result += " ]";
+        }
+        System.out.println(result);       
+    }
+    
+    /**
+     * 
+     * @param state
+     * @param group
+     * @return
+     */
+    private boolean stateExits(State state, HashSet<State> group) {
+        for (Character symbol: this.data.get(state).keySet()) {
+            for (State destination: this.data.get(state).get(symbol)) {
+                if (!group.contains(destination) && group.size() > 1) {                                                        
+                    return true;
+                }
+            }            
+        }
+        return false;
+    }
+    
+    
+    
+    // METHOD TO COMBINE STATESET INTO SINGLE STATE
+//    public DFA constructMinimizedDFA() {
+//        
+//        
+//    }
+    
+    
+    @Override 
+    public String toString() {
+        String result = "----------------NFA-------------------";
+        for (State state: this.data.keySet()) {
+            System.out.println();
+            result += "\n" + state.name + "\t"; 
+            for (Character symbol: this.data.get(state).keySet()) {
+                result += "[" + symbol + ":" + this.transitionSetToString(this.data.get(state).get(symbol)) + "]";
+            }   
+        }        
+        result += "\n" + "--------------------------------------";
+        return result;
+    }
+    
+    /**
+     * 
+     */
+    private String transitionSetToString(HashSet<State> states) {
+        String result = ""; 
+        for (State state: states) {
+            result += " " + state.name;           
+        }
+        return result;
+    } 
+          
+    /**
+     * 
+     *
+     */
+    public static class State {
+
+        private String name;
+        private boolean accept;        
+                
+        public State(String name, boolean accept) {
+            this.name = name;
+            this.accept = accept;
+        }        
+    }
+    
+    /**
+     * 
+     *
+     */
+    public static class Transition {
+        
+        private Character symbol;
+        private State dest; 
+        
+        public Transition(Character symbol, State dest) {
+            this.symbol = symbol; 
+            this.dest = dest;
+        }       
+    }    
+}
---
+++ src/test/nfa/NFATests.java
@@ -0,0 +1,218 @@
+package test.nfa;
+
+import static org.junit.Assert.*;
+import java.util.*;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.rules.Timeout;
+import org.junit.runner.Description;
+
+import main.nfa.*;
+import main.nfa.NFA.State;
+import main.nfa.NFA.Transition;
+
+/**
+ * @author Kira Toal
+ * JUNIT TESTING LAYOUT BY DR. ANDREW FORNEY
+ */
+public class NFATests {
+    
+    // =================================================
+    // Test Configuration
+    // =================================================
+    
+    // Global timeout to prevent infinite loops from
+    // crashing the test suite
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(2);
+    
+    // Used for grading, reports the total number of tests
+    // passed over the total possible
+    static int possible = 0, passed = 0;
+
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+        @Override
+        protected void succeeded(Description description) {
+            passed++;
+        }
+    };
+    
+    @Before
+    public void init () {
+        possible++;
+    }
+    
+    @AfterClass
+    public static void gradeReport () {
+        System.out.println("============================");
+        System.out.println("Tests Complete");
+        System.out.println(passed + " / " + possible + " passed!");
+        if ((1.0 * passed / possible) >= 0.9) {
+            System.out.println("[!] Nice job!"); // Automated acclaim!
+        }
+        System.out.println("============================");
+    }
+    
+    // =================================================
+    // Unit Tests
+    // =================================================
+
+    @Test
+    public void testIsDFA_1() {
+        // Test behavior when alphabet contains λ.
+        NFA myNFA = new NFA(new HashSet<>(Arrays.asList('0', '1', 'λ')), new HashMap<State, HashMap<Character, HashSet<State>>>());
+        NFA.State s1 = new NFA.State("s1", false);
+        NFA.State s2 = new NFA.State("s2", true);
+        
+        myNFA.addState(s1);
+        myNFA.addState(s2);
+        
+        myNFA.addTransition(s1, new NFA.Transition('0', s2));
+        myNFA.addTransition(s1, new NFA.Transition('1', s2));
+        myNFA.addTransition(s2, new NFA.Transition('0', s2));
+        myNFA.addTransition(s2, new NFA.Transition('1', s1));
+        
+        assertFalse(myNFA.isDFA()); 
+    }
+    
+    @Test
+    public void testIsDFA_2() {
+        // Test behavior when NFA does not have a transition for every character.
+        NFA myNFA = new NFA(new HashSet<>(Arrays.asList('0', '1')), new HashMap<State, HashMap<Character, HashSet<State>>>());
+        NFA.State s1 = new NFA.State("s1", true);
+        NFA.State s2 = new NFA.State("s2", false);
+        
+        myNFA.addState(s1);
+        myNFA.addState(s2);
+        
+        myNFA.addTransition(s1, new NFA.Transition('0', s2));
+        myNFA.addTransition(s2, new NFA.Transition('0', s2));
+        myNFA.addTransition(s2, new NFA.Transition('1', s1));
+        
+        assertFalse(myNFA.isDFA()); 
+    }
+
+    @Test
+    public void testIsDFA_3() {
+        // Test behavior when transition has multiple destination states.
+        NFA myNFA = new NFA(new HashSet<>(Arrays.asList('a', 'b')), new HashMap<State, HashMap<Character, HashSet<State>>>());
+        NFA.State s1 = new NFA.State("s1", true);
+        NFA.State s2 = new NFA.State("s2", false);
+        
+        myNFA.addState(s1);
+        myNFA.addState(s2);
+        
+        myNFA.addTransition(s1, new NFA.Transition('a', s2));
+        myNFA.addTransition(s1, new NFA.Transition('a', s1));
+        myNFA.addTransition(s1, new NFA.Transition('b', s2));
+        myNFA.addTransition(s2, new NFA.Transition('a', s2));
+        myNFA.addTransition(s2, new NFA.Transition('b', s1));
+        
+        assertFalse(myNFA.isDFA()); 
+    }
+    
+    @Test
+    public void testIsDFA_4() {
+        // Test isDFA() behavior on valid DFA.
+        NFA myNFA = new NFA(new HashSet<>(Arrays.asList('0', '1', '2')), new HashMap<State, HashMap<Character, HashSet<State>>>());
+        NFA.State s1 = new NFA.State("s1", false);
+        NFA.State s2 = new NFA.State("s2", true);
+        NFA.State s3 = new NFA.State("s3", true);
+
+        
+        myNFA.addState(s1);
+        myNFA.addState(s2);
+        myNFA.addState(s3);
+        
+        myNFA.addTransition(s1, new NFA.Transition('0', s1));
+        myNFA.addTransition(s1, new NFA.Transition('1', s1));
+        myNFA.addTransition(s1, new NFA.Transition('2', s2));
+        myNFA.addTransition(s2, new NFA.Transition('0', s2));
+        myNFA.addTransition(s2, new NFA.Transition('1', s3));
+        myNFA.addTransition(s2, new NFA.Transition('2', s2));
+        myNFA.addTransition(s3, new NFA.Transition('0', s1));
+        myNFA.addTransition(s3, new NFA.Transition('1', s2));
+        myNFA.addTransition(s3, new NFA.Transition('2', s2));
+        
+        assertTrue(myNFA.isDFA()); 
+    }   
+    
+    @Test 
+    public void testAddTransition_1() {
+        // Do not add duplicate transition.
+        NFA myNFA = new NFA(new HashSet<>(Arrays.asList('a', 'b')), new HashMap<State, HashMap<Character, HashSet<State>>>());
+        NFA.State s1 = new NFA.State("s1", true);
+        NFA.State s2 = new NFA.State("s2", false);
+        
+        myNFA.addState(s1);
+        myNFA.addState(s2);
+        
+        myNFA.addTransition(s1, new NFA.Transition('a', s2));
+        myNFA.addTransition(s1, new NFA.Transition('a', s2)); // Attempt to add duplicate.
+        myNFA.addTransition(s1, new NFA.Transition('b', s2));
+        myNFA.addTransition(s2, new NFA.Transition('a', s2));
+        myNFA.addTransition(s2, new NFA.Transition('b', s1));
+        
+        assertTrue(myNFA.isDFA()); 
+    }
+    
+    @Test
+    public void testKEquivalenceHelper_t0() {
+        NFA myNFA = new NFA(new HashSet<>(Arrays.asList('0', '1')), new HashMap<State, HashMap<Character, HashSet<State>>>());
+        NFA.State s0 = new NFA.State("s0", false);
+        NFA.State s1 = new NFA.State("s1", false);
+        NFA.State s2 = new NFA.State("s2", false);
+        NFA.State s3 = new NFA.State("s3", false);
+        NFA.State s4 = new NFA.State("s4", true);
+      
+        myNFA.addState(s0);
+        myNFA.addState(s1);
+        myNFA.addState(s2);
+        myNFA.addState(s3);
+        myNFA.addState(s4);
+        
+        myNFA.addTransition(s0, new NFA.Transition('0', s1));
+        myNFA.addTransition(s0, new NFA.Transition('1', s3));
+        myNFA.addTransition(s1, new NFA.Transition('0', s2));
+        myNFA.addTransition(s1, new NFA.Transition('1', s4));
+        myNFA.addTransition(s2, new NFA.Transition('0', s1));
+        myNFA.addTransition(s2, new NFA.Transition('1', s4));
+        myNFA.addTransition(s3, new NFA.Transition('0', s2));
+        myNFA.addTransition(s3, new NFA.Transition('1', s4));
+        myNFA.addTransition(s4, new NFA.Transition('0', s4));
+        myNFA.addTransition(s4, new NFA.Transition('1', s4));
+        
+        HashSet<HashSet<State>> stateGroups = new HashSet<HashSet<State>>();
+        HashSet<State> acceptStates = new HashSet<State>(Arrays.asList(s4));
+        HashSet<State> nonAcceptStates = new HashSet<State>(Arrays.asList(s0, s1, s2, s3)); 
+        stateGroups.add(acceptStates);
+        stateGroups.add(nonAcceptStates);
+        myNFA.kEquivalenceHelper(stateGroups);
+               
+    }
+    
+//    
+//    @Test 
+//    public void testMinize_1() {
+//        NFA myNFA = new NFA(new HashSet<>(Arrays.asList('a', 'b')), new HashMap<State, HashMap<Character, HashSet<State>>>());
+//        NFA.State s1 = new NFA.State("s1", true);
+//        NFA.State s2 = new NFA.State("s2", false);
+//        
+//        myNFA.addState(s1);
+//        myNFA.addState(s2);
+//        
+//        myNFA.addTransition(s1, new NFA.Transition('a', s2));
+//        myNFA.addTransition(s1, new NFA.Transition('b', s2));
+//        myNFA.addTransition(s2, new NFA.Transition('a', s2));
+//        myNFA.addTransition(s2, new NFA.Transition('b', s1));
+//                
+//        assertTrue(myNFA.isDFA()); 
+//    }
+    
+}
+
